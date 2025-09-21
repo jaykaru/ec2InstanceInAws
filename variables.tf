d538943c3f7ed033c2e2f8beb19e5242fd5dce68
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-09-21 21:34:11 +0100

    variables added for instance type and ami

--- variables.tf
+++ variables.tf
@@ -4,3 +4,14 @@ variable "cidr" {
   default     = "10.0.0.0/16"
 }
 
+variable "instance_type" {
+  description = "The type of EC2 instance"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-046c2381f11878233"
+}
