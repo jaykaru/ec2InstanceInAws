b8eba2265766439470c74565ff46a84de97b6708
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-09-22 12:33:48 +0100

    updated script

--- userdataweb1.sh
+++ userdataweb1.sh
@@ -9,7 +9,6 @@ sudo apt-get install -y curl wget git unzip
 
 # Get the instance ID and write it to a file
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-echo "Instance ID: $INSTANCE_ID" | sudo tee /var/www/html/instance-id.html
 
 # Install Apache web server
 sudo apt-get install -y apache2
@@ -19,4 +18,4 @@ sudo systemctl start apache2
 sudo systemctl enable apache2
 
 # Create a simple index.html
-echo "<h1>Welcome to your Web One EC2 instance!</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Welcome to your Web One EC2 instance with Instance Id $INSTANCE_ID!</h1>" | sudo tee /var/www/html/index.html
